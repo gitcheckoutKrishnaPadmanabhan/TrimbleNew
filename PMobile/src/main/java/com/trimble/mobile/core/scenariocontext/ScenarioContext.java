bc784921f8a12a90dcb55077e86ee0142a839bba
Anitha-Balasubramani <Anitha_Balasubramani@trimble.com>
2019-06-20 12:39:31 +0530

    MAA-24 - Passing Data Between Test Step
    Pass data value between different steps within or across scenario steps in cucumber

---
+++ PMobile/src/main/java/com/trimble/mobile/core/scenariocontext/ScenarioContext.java
@@ -0,0 +1,24 @@
+package com.trimble.mobile.core.scenariocontext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScenarioContext {
+	private  Map<String, Object> scenarioContext;
+
+	public ScenarioContext() {
+		scenarioContext = new HashMap<>();
+	}
+
+	public void setContext(String key, Object value) {
+        scenarioContext.put(key.toString(), value);
+    }
+
+    public Object getContext(String key){
+        return scenarioContext.get(key.toString());
+    }
+
+    public Boolean isContains(String key){
+        return scenarioContext.containsKey(key.toString());
+    }
+}
